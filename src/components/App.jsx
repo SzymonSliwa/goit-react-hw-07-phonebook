8187232c33053d55ee678e5ca9342459c5224301
SzymonSliwa <creedo@tlen.pl>
2023-07-21 00:42:35 +0200

    mockapi hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { selectContacts, selectLoading, selectError } from 'redux/selectors';
+import { fetchContacts } from '../redux/operations';
+
+const App = () => {
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div
+      className="container"
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
         color: '#010101',
       }}
     >
-      Hasta la victoria siempre
+      <h1 className="section_title">Phonebook</h1>
+      <ContactForm />
+      <h2 className="section_title">Contacts </h2>
+      {!contacts.length ? (
+        <p>There are no contacts in your phonebook</p>
+      ) : (
+        <Filter />
+      )}
+      {loading && <p>Loading contacts...</p>}
+      {error && <p>Error {error}</p>}
+      <ContactList />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/operations';
+import { selectContacts, selectContactsFilter } from 'redux/selectors';
+
+export const ContactList = () => {
+  const list = useSelector(selectContacts);
+  const filter = useSelector(selectContactsFilter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <ul>
+        {list
+          .filter(ev => ev.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(contact => {
+            return (
+              <li className="contactItem" key={contact.id}>
+                <p className="contact">{contact.name}:</p>
+                <p className="contact">{contact.phone}</p>
+                <button
+                  type="submit"
+                  onClick={() => dispatch(deleteContact(contact.id))}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
+
+//const getFilteredContacts = () => {
+//  if (!filter) {
+//    return list;
+//  }
+//  return list.filter(contact =>
+//    contact.name.toLowerCase().includes(filter.toLowerCase())
+//  );
+//};
+
+//const filteredContacts = getFilteredContacts();
